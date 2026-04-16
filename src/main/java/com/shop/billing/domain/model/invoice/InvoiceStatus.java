2eb8df442c4454edaa507f1bcff78d1f22a9f82a
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-16 09:59:13 -0300

    Implementando o Domain Model

---
+++ src/main/java/com/shop/billing/domain/model/invoice/InvoiceStatus.java
@@ -0,0 +1,7 @@
+package com.shop.billing.domain.model.invoice;
+
+public enum InvoiceStatus {
+    PAID,
+    UNPAID,
+    CANCELED
+}
---
+++ src/main/java/com/shop/billing/domain/model/invoice/LineItem.java
@@ -0,0 +1,12 @@
+package com.shop.billing.domain.model.invoice;
+
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@EqualsAndHashCode
+public class LineItem {
+    private Integer number;
+    private String name;
+    private BigDecimal amount;
+}
