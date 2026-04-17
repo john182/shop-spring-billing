45aee9b76f86fb51523eafe9eee624572a13fe67
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-17 11:44:32 -0300

    Aprimorando geração de colunas com Hibernate

--- src/main/java/com/shop/billing/domain/model/invoice/PaymentSettings.java
+++ src/main/java/com/shop/billing/domain/model/invoice/PaymentSettings.java
@@ -3,9 +3,7 @@ package com.shop.billing.domain.model.invoice;
 import com.shop.billing.domain.model.DomainException;
 import com.shop.billing.domain.model.IdGenerator;
 import io.micrometer.common.util.StringUtils;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Objects;
@@ -16,8 +14,10 @@ import java.util.UUID;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class PaymentSettings {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
     private UUID creditCardId;
